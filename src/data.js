7d90042a641831755743a30d759110e50efed3de
Jeniffer Genoves <jenifer.genoves03@gmail.com>
2020-06-22 16:10:44 -0400

    LOl

--- src/data.js
+++ src/data.js
@@ -1,9 +1,67 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const alphabetOrder = (firstLetter, condition) => {
+  const result = firstLetter;
+  if (condition === 'a-z') {
+    result.sort((a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      }
+      return -1;
+    });
+  }
+  if (condition === 'z-a') {
+    result.sort((a, b) => {
+      if (a.name < b.name) {
+        return 1;
+      }
+      return -1;
+    });
+  }
+  return result;
 };
-
-export const anotherExample = () => {
-  return 'OMG';
+// Filter
+export const roleFilter = (championsRol, choseFilter) => {
+  const byFilter = championsRol.filter((obj) => {
+    let filterResult = '';
+    for (let i = 0; i < obj.tags.length; i += 1) {
+      if (obj.tags[i] === choseFilter) {
+        filterResult += obj.tags[i];
+      }
+    }
+    return filterResult;
+  });
+  return byFilter;
 };
+// Buscar
+export const namFilt = (data, term) => data.filter(({ name }) => name.toLowerCase().includes(term));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // estas funciones son de ejemplo
+
+
+
+
+// export const example = () => {
+//   return 'example';
+// };
+
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
